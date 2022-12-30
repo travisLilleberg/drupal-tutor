387f98e4a83edc6011c35a35a6aa6d954287df66
Travis Lilleberg <travis.lilleberg@civicactions.com>
2022-12-30 16:30:28 -0700

    Filled out the animal page.

--- modules/custom/zoo/src/Controller/AnimalListController.php
+++ modules/custom/zoo/src/Controller/AnimalListController.php
@@ -3,8 +3,25 @@
 namespace Drupal\zoo\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
 
 class AnimalListController extends ControllerBase {
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  private $conn;
+
+  public function __construct(Connection $conn) {
+    $this->conn = $conn;
+  }
+
+  public static function create(\Symfony\Component\DependencyInjection\ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
   public function listAnimals() {
     $header = [
       $this->t('Name'),
@@ -14,6 +31,16 @@ class AnimalListController extends ControllerBase {
     ];
 
     $rows = [];
+    $results = $this->conn->query("SELECT * FROM {zoo_animal}");
+    foreach ($results as $record) {
+      $age = floor((\Drupal::time()->getRequestTime() - $record->birthday) / (365 * 24 * 3600));
+      $rows[] = [
+        $record->name,
+        $record->type,
+        $this->t('@age years', ['@age' => $age]),
+        $this->t('@weight kg', ['@weight' => $record->weight]),
+      ];
+    }
 
     return [
       '#theme' => 'table',
