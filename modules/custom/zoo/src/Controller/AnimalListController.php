7f590f07deb303874184374fc864ad2cab64b6e2
Travis Lilleberg <travis.lilleberg@civicactions.com>
2022-12-30 17:01:43 -0700

    Added animal by habitat page

--- modules/custom/zoo/src/Controller/AnimalListController.php
+++ modules/custom/zoo/src/Controller/AnimalListController.php
@@ -51,4 +51,47 @@ class AnimalListController extends ControllerBase {
       '#header' => $header,
     ];
   }
+
+  public function listAnimalsInHabitat($habitat) {
+    $header = [
+      $this->t('Name'),
+      $this->t('Type'),
+      $this->t('Age'),
+      $this->t('Weight'),
+    ];
+
+    $rows = [];
+    $results = $this->conn->query(
+      "SELECT * FROM {zoo_animal} WHERE habitat_id = :habitat_id ORDER BY name",
+      ['habitat_id' => $habitat]
+    );
+
+    foreach ($results as $record) {
+      $age = floor((\Drupal::time()->getRequestTime() - $record->birthday) / (365 * 24 * 3600));
+      $rows[] = [
+        $record->name,
+        $record->type,
+        $this->t('@age years', ['@age' => $age]),
+        $this->t('@weight kg', ['@weight' => $record->weight]),
+      ];
+    }
+
+    return [
+      '#theme' => 'table',
+      '#rows' => $rows,
+      '#header' => $header,
+    ];
+  }
+
+  public function listAnimalsInHabitatTitle($habitat) {
+    $name = $this->conn->query(
+      "SELECT name FROM {zoo_habitat} WHERE habitat_id = :habitat_id",
+      ['habitat_id' => $habitat]
+    )->fetchField();
+
+    if (!empty($name)) {
+      return $this->t('Animals in @habitat', ['@habitat' => $name]);
+    }
+    return $this->t('Habitat Not Found');
+  }
 }
