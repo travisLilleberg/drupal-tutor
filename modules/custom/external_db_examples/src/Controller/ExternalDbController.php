3a502fe389e6db35343baeb95378c0f106caf5fc
Travis Lilleberg <travis.lilleberg@civicactions.com>
2023-01-01 14:47:40 -0700

    Created a page to display the data from the personnel external db

---
+++ modules/custom/external_db_examples/src/Controller/ExternalDbController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\external_db_examples\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
+
+class ExternalDbController extends ControllerBase {
+  public function personnelList() {
+    $header = [
+      $this->t('Last Name'),
+      $this->t('First Name'),
+      $this->t('Department'),
+    ];
+    $rows = [];
+
+    $old_db_key = Database::setActiveConnection('personnel');
+    $conn = Database::getConnection();
+
+    $query = $conn->select('employee', 'e');
+    $query->innerJoin('department_employee', 'de', 'e.id = de.employee_id');
+    $query->innerJoin('department', 'd', 'de.department_id = d.id');
+    $query->fields('e', ['lname', 'fname'])
+      ->fields('d', ['name'])
+      ->orderBy('d.name')
+      ->orderBy('e.lname')
+      ->orderBy('e.fname');
+    $results = $query->execute();
+
+    foreach ($results as $record) {
+      $rows[] = [
+        $record->lname,
+        $record->fname,
+        $record->name,
+      ];
+    }
+    Database::setActiveConnection($old_db_key);
+
+    return [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+  }
+}
