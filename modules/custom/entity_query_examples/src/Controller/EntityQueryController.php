1d9ff03fa1dff10888720e047627918c4b89f77f
Travis Lilleberg <travis.lilleberg@civicactions.com>
2023-01-05 14:41:09 -0700

    Created new pages using Drupals EntityQuery functionality.

---
+++ modules/custom/entity_query_examples/src/Controller/EntityQueryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\entity_query_examples\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class EntityQueryController extends ControllerBase {
+  public function userList() {
+    $user_storage = $this->entityTypeManager()->getStorage('user');
+    $results = $user_storage->getQuery()
+      ->condition('name',  'b%', 'LIKE')
+      ->execute();
+    ksm($results);
+    $users = $user_storage->loadMultiple($results);
+
+    $header = [
+      $this->t('Username'),
+      $this->t('Email'),
+    ];
+    $rows = [];
+    foreach ($users as $user) {
+      $rows[] = [
+        $user->getDisplayName(),
+        $user->getEmail()
+      ];
+    }
+
+    return [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+  }
+
+  public function nodeList() {
+    $node_storage = $this->entityTypeManager()->getStorage('node');
+    $results = $node_storage->getQuery()
+      ->condition('type', 'article')
+      ->exists('field_state')
+      ->sort('title')
+      ->execute();
+    $nodes = $node_storage->loadMultiple($results);
+
+    $header = [
+      $this->t('ID'),
+      $this->t('Type'),
+      $this->t('Title'),
+      $this->t('Author'),
+    ];
+    $rows = [];
+    foreach ($nodes as $node) {
+      $rows[] = [
+        $node->id(),
+        $node->bundle(),
+        $node->getTitle(),
+        $node->getOwner()->getDisplayName(),
+      ];
+    }
+
+    return [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+  }
+}
