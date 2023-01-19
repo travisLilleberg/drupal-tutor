e1114fa028468b5cea125f39bf092e99c23e0443
Travis Lilleberg <travis.lilleberg@civicactions.com>
2023-01-19 08:32:56 -0700

    You can use forms inside other render arrays.

--- modules/custom/form_examples/src/Plugin/Block/ContactFormBlock.php
+++ modules/custom/form_examples/src/Plugin/Block/ContactFormBlock.php
@@ -19,6 +19,12 @@ class ContactFormBlock extends BlockBase {
    * @inheritDoc
    */
   public function build() {
-    return \Drupal::formBuilder()->getForm('\Drupal\form_examples\Form\ContactForm');
+    $form = \Drupal::formBuilder()->getForm('\Drupal\form_examples\Form\ContactForm');
+    return [
+      'some_text' => [
+        '#markup' => '<p>' . $this->t('Here is some HTML') . '</p>',
+      ],
+      'form' => $form,
+    ];
   }
 }
