18e68d6236d3b02f35fa5532fa3a31b50235a6a8
Travis Lilleberg <travis.lilleberg@civicactions.com>
2023-01-19 08:19:01 -0700

    Added form_examples module and an example form

---
+++ modules/custom/form_examples/src/Form/ContactForm.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\form_examples\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ContactForm extends FormBase {
+  public function getFormId() {
+    return 'form_examples_contact_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+    ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email Address'),
+      '#required' => TRUE,
+    ];
+    $form['message'] = [
+      '#type' => 'textarea',
+      '#rows' => 10,
+      '#title' => $this->t('Message'),
+      '#required' => TRUE,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Send Message'),
+    ];
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::messenger()->addMessage($this->t('Thanks for Submitting the Form!'));
+  }
+
+}
